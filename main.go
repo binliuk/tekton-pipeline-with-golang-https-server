07889b0e8d167641fbfabc7391fed4ee48ce98cd
Bin Li <binli.uk@outlook.com>
2020-04-25 19:27:22 +0100

    add handler to /

--- main.go
+++ main.go
@@ -1,23 +1,52 @@
 package main
 
 import (
-    // "fmt"
-    // "io"
+    "flag"
+    "html/template"
     "net/http"
     "log"
 )
 
+var addr = flag.String("addr", ":8443", "http service address") 
+var templ = template.Must(template.New("qr").Parse(templateStr))
+
 func HelloServer(w http.ResponseWriter, req *http.Request) {
     w.Header().Set("Content-Type", "text/plain")
     w.Write([]byte("This is an example server.\n"))
-    // fmt.Fprintf(w, "This is an example server.\n")
-    // io.WriteString(w, "This is an example server.\n")
 }
 
 func main() {
+	
+    flag.Parse()
     http.HandleFunc("/hello", HelloServer)
-    err := http.ListenAndServeTLS(":8443", "server.crt", "server.key", nil)
+    http.Handle("/", http.HandlerFunc(QR))
+    err := http.ListenAndServeTLS(*addr, "server.crt", "server.key", nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
     }
 }
+
+func QR(w http.ResponseWriter, req *http.Request) {
+    templ.Execute(w, req.FormValue("s"))
+}
+
+const templateStr = `
+<html>
+<head>
+<title>QR Link Generator</title>
+</head>
+<body>
+{{if .}}
+<img src="http://chart.apis.google.com/chart?chs=300x300&cht=qr&choe=UTF-8&chl={{.}}" />
+<br>
+{{.}}
+<br>
+<br>
+{{end}}
+<form action="/" name=f method="GET">
+    <input maxLength=1024 size=70 name=s value="" title="Text to QR Encode">
+    <input type=submit value="Show QR" name=qr>
+</form>
+</body>
+</html>
+`
